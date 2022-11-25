0665ded46014e2630b7121334a1cdfda55c570c3
Carlo Valenzuela <gusvalenzuela77@gmail.com>
2022-11-25 16:28:40 -0600

    Agregar archivos de proyecto.

---
+++ CardHolder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMachine
+{
+    internal class CardHolder
+    {
+        string _cardNumber;
+        int _pin;
+        string _firstName;
+        string _lastName;
+        double _balance;
+
+        public CardHolder(string cardNumber, int pin, string firstName, string lastName, double balance)
+        {
+            _cardNumber = cardNumber;
+            _pin = pin;
+            _firstName = firstName;
+            _lastName = lastName;
+            _balance = balance;
+        }
+        public string CardNumber
+        {
+            get { return _cardNumber; }
+            set { _cardNumber = value; }
+        }
+        public int Nip
+        {
+            get { return _pin; }
+            set { _pin = value; }
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public double Balance
+        {
+            get { return _balance; }
+            set
+            {
+                _balance = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MoneyMachine
+{
+
+    internal class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            CardHolder user1 = new CardHolder("6250941006528599", 1235, "Carlo Gustavo", "Valenzuela Zepeda", 200);            
+            Action<object> getCardsInfo = (object card) =>
+            {   
+                if (card == null) {
+                    Console.WriteLine("Why this is null?");
+                } else
+                {
+                    Console.WriteLine("This shuldnt work?" + card);
+                }
+                
+                
+            };
+            var fetchInfoTask = new Task(getCardsInfo, user1);
+            fetchInfoTask.Start();
+
+            Console.ReadLine();
+
+        }
+        
+        public static async Task<string> FetchCardInfo()
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://localhost:8000/atm");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                return $"\nException Caught!\nMessage :{e.Message}";
+                
+            }
+        }
+
+    }
+    
+}
